0c65b746d45b646ac4c29e86b374d957a87e9915
lindagustafsson <adnil90@gmail.com>
2024-10-13 21:24:48 +0200

    test: add test for ingredient

---
+++ tests/IngredientTest.java
@@ -0,0 +1,46 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class IngredientTest {
+    private IIngredient ingredient;
+
+    @BeforeEach
+    public void setUp() {
+        ingredient = new Ingredient(1, 8.5f, "dl", "Multisodiumglutamat");
+    }
+
+  @Test
+  void shouldReturnValues() {
+      assertEquals(1, ingredient.getId());
+      assertEquals(8.5f, ingredient.getAmount());
+      assertEquals("dl", ingredient.getMeasurement());
+      assertEquals("Multisodiumglutamat", ingredient.getName());
+  }
+
+  @Test
+    void shouldReturnFormattedString() {
+        assertEquals("8.5 dl Multisodiumglutamat", ingredient.toString());
+  }
+
+  @Test
+    void shouldSetAmount() {
+        ingredient.setAmount(3);
+        assertEquals(3, ingredient.getAmount());
+        assertEquals("3 dl Multisodiumglutamat", ingredient.toString());
+  }
+
+  @Test
+  void shouldSetMeasurement() {
+        ingredient.setMeasurement("cl");
+        assertEquals("cl", ingredient.getMeasurement());
+        assertEquals("8.5 cl Multisodiumglutamat", ingredient.toString());
+  }
+
+  @Test
+    void shouldSetName() {
+        ingredient.setName("Salt");
+        assertEquals("Salt", ingredient.getName());
+        assertEquals("8.5 dl Salt", ingredient.toString());
+  }
+}
