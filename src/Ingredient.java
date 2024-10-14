73847bf4262c61c1c31c465371e49fdc9d6f9f22
lindagustafsson <adnil90@gmail.com>
2024-10-14 20:10:00 +0200

    feat: add ingredient class

---
+++ src/Ingredient.java
@@ -0,0 +1,63 @@
+import interfaces.IIngredient;
+
+public class Ingredient implements IIngredient {
+    private final int id;
+    private float amount;
+    private String measurement;
+    private String name;
+
+    public Ingredient(int id, float amount, String measurement, String name) {
+        this.id = id;
+        this.amount = amount;
+        this.measurement = measurement;
+        this.name = name;
+    }
+
+    public Ingredient(float amount, String measurement, String name) {
+        this(-1, amount, measurement, name);
+    }
+
+    @Override
+    public int getId() {
+        return this.id;
+    }
+
+    @Override
+    public float getAmount() {
+        return this.amount;
+    }
+
+    @Override
+    public void setAmount(float amount) {
+        this.amount = amount;
+    }
+
+    @Override
+    public String getMeasurement() {
+        return this.measurement;
+    }
+
+    @Override
+    public void setMeasurement(String measurement) {
+        this.measurement = measurement;
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        if (this.amount == Math.floor(this.amount)) {
+            return String.format("%.0f %s %s", this.amount, this.measurement, this.name);
+        } else {
+            return String.format("%.1f %s %s", this.amount, this.measurement, this.name);
+        }
+    }
+}
--- tests/IngredientTest.java
+++ tests/IngredientTest.java
@@ -1,8 +1,8 @@
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import interfaces.IIngredient;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class IngredientTest {
     private IIngredient ingredient;
@@ -12,37 +12,46 @@ public class IngredientTest {
         ingredient = new Ingredient(1, 8.5f, "dl", "Multisodiumglutamat");
     }
 
-  @Test
-  void shouldReturnValues() {
-      assertEquals(1, ingredient.getId());
-      assertEquals(8.5f, ingredient.getAmount());
-      assertEquals("dl", ingredient.getMeasurement());
-      assertEquals("Multisodiumglutamat", ingredient.getName());
-  }
+    @Test
+    void shouldAllowOverloadingWithDefaultId() {
+        ingredient = new Ingredient(8.5f, "dl", "Multisodiumglutamat");
+        assertEquals(-1, ingredient.getId());
+        assertEquals(8.5f, ingredient.getAmount());
+        assertEquals("dl", ingredient.getMeasurement());
+        assertEquals("Multisodiumglutamat", ingredient.getName());
+    }
 
-  @Test
+    @Test
+    void shouldReturnValues() {
+        assertEquals(1, ingredient.getId());
+        assertEquals(8.5f, ingredient.getAmount());
+        assertEquals("dl", ingredient.getMeasurement());
+        assertEquals("Multisodiumglutamat", ingredient.getName());
+    }
+
+    @Test
     void shouldReturnFormattedString() {
         assertEquals("8.5 dl Multisodiumglutamat", ingredient.toString());
-  }
+    }
 
-  @Test
+    @Test
     void shouldSetAmount() {
         ingredient.setAmount(3);
         assertEquals(3, ingredient.getAmount());
         assertEquals("3 dl Multisodiumglutamat", ingredient.toString());
-  }
+    }
 
-  @Test
-  void shouldSetMeasurement() {
+    @Test
+    void shouldSetMeasurement() {
         ingredient.setMeasurement("cl");
         assertEquals("cl", ingredient.getMeasurement());
         assertEquals("8.5 cl Multisodiumglutamat", ingredient.toString());
-  }
+    }
 
-  @Test
+    @Test
     void shouldSetName() {
         ingredient.setName("Salt");
         assertEquals("Salt", ingredient.getName());
         assertEquals("8.5 dl Salt", ingredient.toString());
-  }
+    }
 }
