585e71ad97714b47fae870993b9be88ef2420dfd
lindagustafsson <adnil90@gmail.com>
2024-10-15 14:13:19 +0200

    feat: add find ingredient method

--- src/interfaces/IRecipe.java
+++ src/interfaces/IRecipe.java
@@ -4,17 +4,25 @@ import java.util.ArrayList;
 
 public interface IRecipe {
     int getId();
+
     String getCategory();
 
     String getTitle();
+
     void setTitle(String title);
 
     String getInstructions();
+
     void setInstructions(String instructions);
 
     ArrayList<IIngredient> getIngredients();
+
+    IIngredient findIngredient(int id);
+
     void insertIngredient(IIngredient ingredient);
+
     void updateIngredient(IIngredient ingredient);
+
     void deleteIngredient(int id);
 
     String toString();
