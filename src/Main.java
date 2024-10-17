f55394383d1e4bce0e559c3b5d94b1b0f71b9d8c
lindagustafsson <adnil90@gmail.com>
2024-10-17 23:08:16 +0200

    feat: start using file handler for recipe collection

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
+import interfaces.IRecipeCollection;
+
 public class Main {
     public static void main(String[] args) {
-        RecipeCollection recipeCollection = new RecipeCollection();
+        IRecipeCollection recipeCollection = new RecipeCollection("recipes.ser");
         MenuHandler menuHandler = new MenuHandler(recipeCollection);
         menuHandler.handle();
     }
