382ace3a721e052ca0b7b58b0a7b3374e5c4bc6a
lindagustafsson <adnil90@gmail.com>
2024-10-17 21:46:46 +0200

    feat: add collection and handler to main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        RecipeCollection recipeCollection = new RecipeCollection();
+        MenuHandler menuHandler = new MenuHandler(recipeCollection);
+        menuHandler.handle();
     }
 }
